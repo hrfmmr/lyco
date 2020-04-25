fb8028e54730f6790c972272581bc1ecc9f273c0
hrfmmr <ladiciushm@gmail.com>
2020-04-25 15:03:05 +0900

    Add one-line editor

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/hrfmmr/lyco
 
 go 1.14
 
-require github.com/gcla/gowid v1.1.0
+require (
+	github.com/gcla/gowid v1.1.0
+	github.com/gdamore/tcell v1.3.0
+	github.com/sirupsen/logrus v1.5.0
+)
--- main.go
+++ main.go
@@ -1,34 +1,99 @@
 package main
 
 import (
+	"bytes"
+	"io"
+
 	"github.com/gcla/gowid"
 	"github.com/gcla/gowid/examples"
 	"github.com/gcla/gowid/widgets/dialog"
 	"github.com/gcla/gowid/widgets/edit"
 	"github.com/gcla/gowid/widgets/holder"
 	"github.com/gcla/gowid/widgets/text"
+	"github.com/gdamore/tcell"
+	"github.com/hrfmmr/lyco/appkeys"
+
+	"github.com/sirupsen/logrus"
 )
 
 //======================================================================
 
 var (
-	app   *gowid.App
-	err   error
-	input *dialog.Widget
+	app    *gowid.App
+	err    error
+	inpute *edit.Widget
+	inputd *dialog.Widget
 )
+
+type (
+	handler struct{}
+)
+
+func (h handler) UnhandledInput(app gowid.IApp, ev interface{}) bool {
+	handled := false
+	if evk, ok := ev.(*tcell.EventKey); ok {
+		logrus.Infof("🐛 UnhandledInput evk:%v", evk.Key())
+		switch evk.Key() {
+		case tcell.KeyEsc, tcell.KeyCtrlC:
+			logrus.Infof("🐛 case tcell.KeyEnter, tcell.KeyCtrlC ev:%v", ev)
+			handled = true
+			app.Quit()
+		case tcell.KeyCtrlX:
+			logrus.Infof("🐛 case tcell.KeyCtrlX:")
+		case tcell.KeyEnter:
+			//TODO: dialogに対するEnterキー押下で入力値をログ出力しようとしたが、ダメ。(dialog表示時にはこのUnhandledInput自体フックされず。)
+			// dialog自体に対するイベントフックAPIがないか調査する🔍
+			logrus.Infof("🐛 case tcell.KeyEnter:")
+			handled = true
+			var buf bytes.Buffer
+			_, err := io.Copy(&buf, inpute)
+			if err != nil {
+				logrus.Fatal(err)
+			}
+			logrus.Infof("🐛 editor buf:%v", buf.String())
+		}
+	}
+	return handled
+}
+
+func handleEnter() appkeys.KeyInputFn {
+	return func(ev *tcell.EventKey, app gowid.IApp) bool {
+		handled := false
+		switch ev.Key() {
+		case tcell.KeyEnter:
+			logrus.Infof("🐛 main#handleEnter case tcell.KeyEnter")
+			var buf bytes.Buffer
+			_, err := io.Copy(&buf, inpute)
+			if err != nil {
+				logrus.Fatal(err)
+			}
+			logrus.Infof("🐛 main#handleEnter case tcell.KeyEnter - 📝editor buf:%v", buf.String())
+			inputd.Close(app)
+			handled = true
+		}
+		return handled
+	}
+}
 
 func main() {
 	f := examples.RedirectLogger("editor.log")
 	defer f.Close()
 
-	editWidget := edit.New()
+	inpute = edit.New()
+	onelineEd := appkeys.New(
+		inpute,
+		handleEnter(),
+		appkeys.Options{
+			ApplyBefore: true,
+		})
 	txt := text.New("hello, world")
 	viewHolder := holder.New(txt)
-	input = dialog.New(editWidget)
-	input.Open(viewHolder, gowid.RenderWithRatio{R: 0.5}, app)
+	inputd = dialog.New(onelineEd)
+	inputd.Open(viewHolder, gowid.RenderWithRatio{R: 0.5}, app)
 	app, err = gowid.NewApp(gowid.AppArgs{
 		View: viewHolder,
+		Log:  logrus.StandardLogger(),
 	})
 	examples.ExitOnErr(err)
-	app.SimpleMainLoop()
+	app.MainLoop(handler{})
 }
